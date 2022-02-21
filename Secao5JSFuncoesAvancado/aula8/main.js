b1c2c5477635973b77cae78eff4d691f0a746990
Gaqq41229 <66617480+Gaqq41229@users.noreply.github.com>
2022-02-21 20:36:27 -0300

    0508 forEach()

--- Secao5JSFuncoesAvancado/aula8/main.js
+++ Secao5JSFuncoesAvancado/aula8/main.js
@@ -32,3 +32,10 @@ tamanhoDoNome = (element, index, array) => {
 };
 
 // pessoas.forEach(tamanhoDoNome);
+
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+let total = 0;
+a1.forEach((valor) => {
+  total += valor;
+});
+console.log(total);
