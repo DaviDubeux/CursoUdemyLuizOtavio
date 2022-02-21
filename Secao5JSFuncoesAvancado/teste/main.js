fb056d72ef0292837a18afbf4de7a83c0c3cd6a0
Gaqq41229 <66617480+Gaqq41229@users.noreply.github.com>
2022-02-21 20:30:07 -0300

    0507 filter + map + reduce

---
+++ Secao5JSFuncoesAvancado/teste/main.js
@@ -0,0 +1,34 @@
+/* 
+
+arr.forEach(callback(currentValue [, index [, array]])[, thisArg]);
+callback
+    Função para executar em cada elemento, recebendo três argumentos:
+    
+    currentValue:
+        O valor atual do elemento sendo processado no array.
+
+    index (opcional):
+        O índice do elemento atual sendo processado no array.
+
+    array (opcional):
+        O array que `forEach()` está sendo aplicado.
+
+thisArg (opcional):
+    Opcional. Valor a ser usado como this quando executar callback.
+
+*/
+
+const pessoas = [
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduardo", idade: 55 },
+  { nome: "Letícia", idade: 10 },
+  { nome: "Rosana", idade: 64 },
+  { nome: "Wallace", idade: 63 },
+];
+
+tamanhoDoNome = (element, index, array) => {
+  console.log(`o nome de pessoas[${index}] tem ${element.nome.length} letras`);
+};
+
+// pessoas.forEach(tamanhoDoNome);
